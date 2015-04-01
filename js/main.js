ec352b23ec557c51b47395945c18d339acbc1592
Hou Liwen <liwen_zhe@Hous-MacBook-Air-2.local>
2015-04-01 22:55:22 +0800

    push to the repo

---
+++ js/main.js
@@ -0,0 +1,50 @@
+//fade in effects
+$("#main-archi").fadeIn(500);
+$("#main-fashion").delay(300).fadeIn(500);
+$("#main-interior").delay(600).fadeIn(500);
+$("#main-lifestyle").delay(900).fadeIn(500);
+
+$("#fashion").on("singletap",function() {
+    $("#cat1").fadeIn(500);
+    $("#cat2").delay(300).fadeIn(500);
+    $("#cat3").delay(600).fadeIn(500);
+    $("#cat4").delay(900).fadeIn(500);
+    $("#cat5").delay(1200).fadeIn(500);
+}); 
+
+//add comments
+$("#send").on("singletap", function() {
+    var nickname = $("#name-holder").val();
+    var content = $ ("#comment-holder").val();
+    if (nickname.length < 1) {
+        nickname = "Anonymous";
+    }
+    if (content.length < 1) {
+        $.UIPopup({
+            id: "note-new-warning",
+            title: 'Content is empty!',
+            message: 'Do not leave empty comment.',
+            continueButton: 'OK',
+            callback: function() {
+                content = ""; 
+            }
+        });
+    }else{
+        addComment(nickname, content);
+    } 
+
+});
+
+function addComment(nickname, content) {
+    $("#old-comment").append("<li>" + "<h3>" + nickname + "</h3>" + "<h4>" + content + "</h4>" + "</li>");
+    $("#name-holder").val("");
+    $("#comment-holder").val("");
+}
+
+//change font size in settings
+$('#rangeValue').html($('#changeFontSize').val());
+$('#changeFontSize').on('change', function() {
+    var fontSize = $(this).val();
+    $('#rangeValue').html($(this).val());
+    $('.article-text').css("font-size", fontSize+"px");
+});
